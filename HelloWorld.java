d390cd45b5f7c68f9a3553bf6731121711df7ba4
pixel97 <dominikpixel@gmail.com>
2019-10-05 15:43:06 +0200

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+       printText("Cześć Minio");
+    }
+
+    private static void printText(String text) {
+        System.out.println(text);
+    }
+    
+
+}
