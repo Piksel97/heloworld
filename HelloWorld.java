9cb0ca7da4e6139425290a16267af8b13dde1fe5
pixel97 <dominikpixel@gmail.com>
2019-10-05 15:57:59 +0200

    added printing number

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
        printText("Cześć Minio");
+       second(10);
     }
 
     private static void printText(String text) {
         System.out.println(text);
     }
-    
+    public static void second(int number){
+        System.out.println(number);
+    }
 
 }
